931440e3212073bd1c154e49fd13ab6803f13819
Sera De La Rosa <sera.delarosa@outlook.com>
2025-02-18 03:21:15 -0500

    Updated app.listen and exported app.

--- server.js
+++ server.js
@@ -124,6 +124,8 @@ app.delete('/cart/:id', async (req, res) => {
 
 //=======================================================
 
-app.listen(3000, () => {
-    console.log('listening on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running...');
 });
+
+module.exports = app; 
